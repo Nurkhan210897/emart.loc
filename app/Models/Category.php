ec16bcbaaf31fa5b66c849aeaafa3a53fd9ce3a3
Darkhan <31818912+Dark-Han@users.noreply.github.com>
2021-02-25 23:44:41 +0600

    добавление slug

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,13 +3,37 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
-
+/**
+ * Class Category
+ * @package App\Models
+ */
 class Category extends Model
 {
+    use Sluggable;
+
+    /**
+     * @var bool
+     */
     public $timestamps=false;
 
+    /**
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function subCategories(){
-        return $this->hasMany('App\Models\SubCategory');
+        return $this->hasMany('App\Models\Category');
     }
 }
