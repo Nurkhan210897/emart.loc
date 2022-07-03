3444f5516e476fd2409f2e27717d13f5f5e3c2b6
Nurhanfront <front@studionomad.kz>
2022-07-03 14:20:23 +0600

    feat: new styles to product page

--- resources/views/rentTerms.blade.php
+++ resources/views/rentTerms.blade.php
@@ -1,5 +1,28 @@
 @extends('layout')
 
 @section('main')
-rentTerms
+    <div class="questions">
+        <div class="container">
+            <div class="title-page">
+                <h1>Условия аренды
+                </h1>
+                <p>Вся информация об условиях аренды
+                </p>
+            </div>
+            <div class="questions_content">
+                <div class="question">
+                    <div class="question_header">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <button class="btn plus">
+                            <img src="/storage/img/plus.svg" alt="">
+                        </button>
+                    </div>
+                    <div class="question_description">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi beatae nesciunt vero fuga
+                            temporibus minus.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
